7e3643aa8f115de70e567397afac61a8725f3286
Glib Rulev <velzepooz@gmail.com>
2020-06-18 08:40:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const rows = tbody.rows;
+
+thead.addEventListener('click', (e) => {
+  const indexOfColumn = e.target.cellIndex;
+
+  const sorted = [...rows].sort((prevRow, currentRow) => {
+    const firstElement = prevRow.cells[indexOfColumn].textContent;
+    const secondElement = currentRow.cells[indexOfColumn].textContent;
+
+    return firstElement.localeCompare(secondElement);
+  });
+
+  tbody.append(...sorted);
+});
